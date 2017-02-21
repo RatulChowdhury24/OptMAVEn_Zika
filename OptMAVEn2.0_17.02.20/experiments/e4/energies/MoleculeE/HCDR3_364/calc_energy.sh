30e2b6092927d92dc31c73de88d74dfe49a7560e
mfa5147 <mfa5147@lionxf.rcc.psu.edu>
2017-02-20 21:37:34 -0500

    OptMAVEn 2.0, as of 20 Feb, 2017: cleaned and functional.

---
+++ OptMAVEn2.0_17.02.20/experiments/e4/energies/MoleculeE/HCDR3_364/calc_energy.sh
@@ -0,0 +1,15 @@
+#PBS -l pmem=4gb
+#PBS -l walltime=23:00:00
+#PBS -j oe
+#PBS -q lionxf
+
+set -u
+echo " "
+echo " "
+echo "Job started on $(hostname -s) at $(date)"
+cd /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/experiments/e4/energies/MoleculeE/HCDR3_364
+vmd -dispdev none -e /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/modules/merge_antigen_part.tcl -args /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/experiments/e4/ /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/experiments/e4/structures/MoleculeE.pdb /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/databases/MAPs/HCDR3/HCDR3_364.pdb /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/experiments/e4/energies/MoleculeE/HCDR3_364/MoleculeE /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/input_files/top_all27_prot_na.rtf
+vmd -dispdev none -e /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/modules/interaction_energies.tcl -args /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/experiments/e4/energies/MoleculeE/HCDR3_364/MoleculeE.psf /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/experiments/e4/energies/MoleculeE/HCDR3_364/MoleculeE.pdb /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/experiments/e4/input_files/positions.dat /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/experiments/e4/energies/MoleculeE/HCDR3_364/energies.dat /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/experiments/e4/Experiment_Details.txt /home/matthew/Maranas_Lab/IPRO_Suite_OptMAVEn2.0/input_files/par_all27_prot_na.prm
+echo " "
+echo "Job ended at $(date)"
+echo " "
